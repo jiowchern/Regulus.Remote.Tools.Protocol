d56c72d8f6caae6996565cdb5d1f5358ceb77210
jc <jiowchern@yahoo.com.tw>
2021-08-29 04:58:16 +0800

    unittest runnable.

--- Regulus.Remote.Tools.Protocol.Sources.Tests/UnitTest1.cs
+++ Regulus.Remote.Tools.Protocol.Sources.Tests/UnitTest1.cs
@@ -1,3 +1,8 @@
+using System.Collections.Immutable;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Text;
 using NUnit.Framework;
 
 namespace Regulus.Remote.Tools.Protocol.Sources.TestProject
@@ -5,6 +10,11 @@ namespace Regulus.Remote.Tools.Protocol.Sources.TestProject
 }
 namespace Regulus.Remote.Tools.Protocol.Sources.Tests
 {
+    using Microsoft.CodeAnalysis.Testing.Verifiers;
+
+    using VerifyCS = CSharpSourceGeneratorVerifier<Regulus.Remote.Tools.Protocol.Sources.SourceGenerator>;
+
+
     public class Tests
     {
         [SetUp]
@@ -13,8 +23,27 @@ namespace Regulus.Remote.Tools.Protocol.Sources.Tests
         }
 
         [Test]
+        public async Task Test2()
+        {
+            var code = @"interface IAA{}";
+            var generated = "interface IAA{}";
+            await new VerifyCS.Test
+            {
+                TestState =
+                {
+                    Sources = { code },
+                    GeneratedSources =
+                    {
+                        (typeof(SourceGenerator), "GeneratedFileName", SourceText.From(generated, Encoding.UTF8, SourceHashAlgorithm.Sha256)),
+                    },ReferenceAssemblies = ReferenceAssemblies.Default.AddAssemblies(ImmutableArray.Create("Regulus.Remote.Tools.Protocol.Sources"))
+                },
+            }.RunAsync();
+        }
+
+        [Test]
         public void Test1()
         {
+            
             var entry = new Regulus.Remote.Tools.Protocol.Sources.TestProject.Entry();
             IProtocol protocol = Regulus.Remote.Tools.Protocol.Sources.TestCommon.ProtocolProvider.Create();
             var service = new Regulus.Remote.Standalone.Service(entry, protocol);
