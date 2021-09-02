706941f13389896a6868fcdba79b561d988f8ee8
jc <jiowchern@yahoo.com.tw>
2021-09-03 05:25:58 +0800

    serializable extractor 3 test.

--- Regulus.Remote.Tools.Protocol.Sources.Tests/ProtocolBuilderTests.cs
+++ Regulus.Remote.Tools.Protocol.Sources.Tests/ProtocolBuilderTests.cs
@@ -12,6 +12,76 @@ namespace Regulus.Remote.Tools.Protocol.Sources.Tests
     {
 
         [Test]
+        public async Task SerializableExtractor3Test()
+        {
+            var source = @"
+namespace NS1
+{
+    public class CReturn
+    {
+        public CReturn Field1;
+    }
+    public class CUseless
+    {
+        public CArg1 Field1;
+    }
+    public enum ENUM1
+    {
+        ITEM1,
+    }
+    public struct CArg3
+    {
+        public ENUM1 Field2;
+        public string Field1;
+    }
+    public struct CArg2
+    {
+        public CArg2 Field3;
+        public ENUM1 Field2;
+        public string Field1;
+    }
+    public class CArg1
+    {
+        public CArg2 Field1;
+    }
+    public interface IB
+    {
+        event System.Action<float , CArg1,CArg3[][]> Event1;
+        Regulus.Remote.Property<string> Property1 {get;}
+    }
+    public interface IA
+    {
+        Regulus.Remote.Value<CReturn> Method(CArg1 a1,int a2,System.Guid id);
+        Regulus.Remote.Notifier<IB> Property1 {get;}
+    }
+}
+
+";
+            var tree = CSharpSyntaxTree.ParseText(source);
+            Compilation compilation = tree.Compilation();
+            var symbols = new SerializableExtractor(compilation).Symbols.Select(s=>s.ToDisplayString());
+           
+            var cSymbols = new[]
+            {
+                "NS1.CReturn",
+                "NS1.CArg1",
+                "NS1.CArg2",
+                "NS1.CArg3",
+                "NS1.CArg3[]",
+                "NS1.CArg3[][]",
+                "int",
+                "string",
+                "System.Guid",
+                "float",
+                "NS1.ENUM1",
+            };
+           
+            var count = symbols.Except(cSymbols).Count();
+            NUnit.Framework.Assert.AreEqual(0, count);
+        }
+
+
+        [Test]
         public async Task SerializableExtractor1Test()
         {
             var source = @"
@@ -40,7 +110,7 @@ namespace NS1
 ";
             var tree=CSharpSyntaxTree.ParseText(source);
             Compilation compilation =tree.Compilation();
-            IEnumerable<INamedTypeSymbol> symbols = new SerializableExtractor(compilation).Symbols;
+            var symbols = new SerializableExtractor(compilation).Symbols;
 
             var cSymbols = new[]
             {
@@ -60,13 +130,11 @@ namespace NS1
 {
     public struct C2
     {
-        public int F1;
-        public string F2;
-        public float F3;
+        
     }
     public class C1
     {
-        public C2;
+        public C2 Field1;
     }
     public interface IB{
         Regulus.Remote.Property<C1> Property1 {get;}
@@ -81,7 +149,7 @@ namespace NS1
 ";
             var tree = CSharpSyntaxTree.ParseText(source);
             Compilation compilation = tree.Compilation();
-            IEnumerable<INamedTypeSymbol> symbols = new SerializableExtractor(compilation).Symbols;
+            var  symbols = new SerializableExtractor(compilation).Symbols;
 
             var cSymbols = new[]
             {
--- Regulus.Remote.Tools.Protocol.Sources/SerializableExtractor.cs
+++ Regulus.Remote.Tools.Protocol.Sources/SerializableExtractor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
@@ -7,22 +8,117 @@ namespace Regulus.Remote.Tools.Protocol.Sources
     using System.Linq;
     class SerializableExtractor
     {
-        public readonly IReadOnlyCollection<INamedTypeSymbol> Symbols;
-
+        public readonly IReadOnlyCollection<ITypeSymbol> Symbols;
+        private readonly Compilation _Compilation;
         public SerializableExtractor(Compilation compilation)
         {
+            _Compilation = compilation;
 
-            
-            var typeSyntaxs = 
+            var symbols = 
                 from tree in compilation.SyntaxTrees
                 let semanticModel = compilation.GetSemanticModel(tree)
                 from node in tree.GetRoot().DescendantNodes()
-                let symbol = semanticModel.GetDeclaredSymbol(node) as INamedTypeSymbol
-                where symbol!=null &&(symbol.TypeKind == TypeKind.Class || symbol.TypeKind == TypeKind.Enum || symbol.TypeKind == TypeKind.Array || symbol.TypeKind == TypeKind.Struct) 
+                let symbol = semanticModel.GetDeclaredSymbol(node) //as INamedTypeSymbol
+                where symbol != null
                 select symbol;
 
-            Symbols = new HashSet<INamedTypeSymbol>(typeSyntaxs);
+            //var namedSymbols = symbols.OfType<INamedTypeSymbol>().ToArray();
+            var fieldSymbols = symbols.OfType<IFieldSymbol>().ToArray();
+            var propertySymbols = symbols.OfType<IPropertySymbol>().ToArray();
+            var eventSymbols = symbols.OfType<IEventSymbol>().ToArray();
+            var methodSymbols = symbols.OfType<IMethodSymbol>().ToArray();
+            var paramSymbols = symbols.OfType<IParameterSymbol>().ToArray();
+
+
+            var paramNameds=_GetNameds(paramSymbols);
+            var methodNameds = _GetNameds(methodSymbols);
+            var fieldNameds = _GetNameds(fieldSymbols);
+
+            var eventNameds = _GetNameds(eventSymbols);
+            var propertyNameds = _GetNameds(propertySymbols);
+            Symbols = new HashSet<ITypeSymbol>(paramNameds.Union(methodNameds).Union(fieldNameds).Union(eventNameds).Union(propertyNameds));
 
         }
+        private IEnumerable<ITypeSymbol> _GetNameds(IEnumerable<IPropertySymbol> symbols)
+        {
+            return _GetNameds(from symbol in symbols
+                let type = symbol.Type as INamedTypeSymbol
+                where type != null
+                where type != null
+                      && _Compilation.GetTypeByMetadataName("Regulus.Remote.Property`1") == type.OriginalDefinition
+                from arg in type.TypeArguments
+
+                select arg);
+        }
+
+        private IEnumerable<ITypeSymbol> _GetNameds(IEnumerable<ITypeSymbol> symbols)
+        {
+            var nameds = symbols.OfType<INamedTypeSymbol>() ;
+            var arrays = symbols.OfType<IArrayTypeSymbol>();
+            return nameds.Union(_GetNameds(arrays));
+        }
+
+        private IEnumerable<ITypeSymbol> _GetNameds(IEnumerable<IArrayTypeSymbol> symbols)
+        {
+            foreach (var symbol in symbols)
+            {
+                yield return symbol;
+                
+                if (symbol is IArrayTypeSymbol arraySymbol)
+                {
+                    foreach (var typeSymbol in _GetNameds(new[] { arraySymbol.ElementType }))
+                    {
+                        yield return typeSymbol;
+                    }
+                }
+                else if(symbol is INamedTypeSymbol named)
+                {
+               
+                    yield return named;
+                }
+                    
+            }
+        }
+
+        
+
+        private IEnumerable<ITypeSymbol> _GetNameds(IEnumerable<IEventSymbol> event_symbols)
+        {
+            return _GetNameds(from symbol in event_symbols
+                let type = symbol.Type as INamedTypeSymbol
+                where type != null
+                from typeArgument in type.TypeArguments
+              
+                select typeArgument);
+        }
+
+        IEnumerable<ITypeSymbol> _GetNameds(IEnumerable<IParameterSymbol> args)
+        {
+            return _GetNameds(from arg in args
+                let type = arg.Type 
+                   
+                    select type);
+        }
+
+        IEnumerable<ITypeSymbol> _GetNameds(IEnumerable<IMethodSymbol> symbols)
+        {
+            return _GetNameds(from symbol in symbols
+                   let type = symbol.ReturnType as INamedTypeSymbol
+
+                    where type !=null && _Compilation.GetTypeByMetadataName("Regulus.Remote.Value`1") == type.OriginalDefinition
+                   from arg in type.TypeArguments   
+                   let argNamed = arg 
+                   select argNamed);
+        }
+
+        IEnumerable<ITypeSymbol> _GetNameds(IEnumerable<IFieldSymbol> symbols)
+        {
+            return _GetNameds(from arg in symbols
+                   let type = arg.Type 
+               
+                select type);
+        }
+
+
     }
 }
